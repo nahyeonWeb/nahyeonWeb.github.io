46e6b99680310da466a85c1bb2bb334b705b15d4
nahyunlim <strongnahyun@gmail.com>
2017-05-14 18:41:33 +0900

    timetable모달 내부 내용 입력 스타일제작 party 모달 내부 문자 스타일,배치 변경

--- portfolio/cultureDay/js/src/timetable.js
+++ portfolio/cultureDay/js/src/timetable.js
@@ -16,9 +16,9 @@
     tmbox.fadeIn();
     tmbox.find('div').eq(btnLi).fadeIn();
     
-    tmbox.find('div').prepend('<button type="button" class="tmClose"><i class="fa fa-times" aria-hidden="true"></i></button>');
+    tmbox.children('div').prepend('<button type="button" class="tmClose"><i class="fa fa-times" aria-hidden="true"></i></button>');
     
-    $('.tmClose').css({width:'3.5em', height:'3.5em', position:'absolute', top: '0.2em', right:'0.2em', zIndex:500, color:'#faa', fontWeight:'bold'});
+    $('.tmClose').css({width:'3.5em', height:'3.5em', position:'absolute', top: '0.2em', right:'0.2em', zIndex:500, color:'#aaa', fontWeight:'bold'});
     $('.tmClose').parent().css({position:'relative'});  
    
    var tmClose = $('.tmClose'); 
@@ -30,4 +30,30 @@
     });
   });
   
+  // 오시는길 모달 탭메뉴
+   
+  
+  var tabLi = $('.tab').find('li');
+   var tabInDiv = $('.tab_box').children('div');
+      
+      tabLi.eq(0).addClass('active');// 최초 설
+      tabInDiv.eq(0).addClass('active_box');// 최초 설
+      
+      tabLi.on('click', function(e){
+        e.preventDefault();
+        // ----------------------------------------------------
+        var $this = $(this); // 선택한 li
+        var indexLi = $this.index(); // 선택 li의 순서
+        console.log(indexLi);// console에 확인
+        
+        tabLi.removeClass('active'); // .tab안의 li에 .active를 제거
+        $this.addClass('active'); // 선택한 li에 .active를 추가
+        
+        // ----------------------------------------------------
+        
+        //.tab_box안의 div에 .active_box 제거
+         tabInDiv.removeClass('active_box'); 
+        //선택된 순서에 맞는 div에 .active_box를 추가
+         tabInDiv.eq(indexLi).addClass('active_box');
+        });
 })(this.jQuery);
